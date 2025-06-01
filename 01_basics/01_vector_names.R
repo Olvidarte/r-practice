705a3fdbd1c0df26e19565d45f6839e21a6edd15
Olvidarte <olvidarteloo@gmail.com>
2025-06-01 21:12:22 +0800

    feat: add vector name attribution practice

--- 01_basics/01_vector_names.R
+++ 01_basics/01_vector_names.R
@@ -3,10 +3,11 @@
 #Date: 2025-05-31
 #Purpose: Represent average days lost per injury using a named vector
 #Topic: Vector, name attribution, indexing
+#Coursera: R Programming > 第一單元 > Data Types - Names Attribute
+################################################################################
 
-  
 #建立一個對應傷害診斷對應傷停時間的向量
 time_loss <- c("ACL" = 270, "Ankle Sprain" = 21, "Hamstring Strain" = 28)
 
-#印出向量內容
+#測試
 print(time_loss)
