5cf5e126d0deeaf46fc09c88ee0918abec518b36
Olvidarte <olvidarteloo@gmail.com>
2025-06-02 10:42:22 +0800

    fix: add background to better understanding when to use dput and dget

--- 01_basics/07_deput_deget_practice.R
+++ 01_basics/07_deput_deget_practice.R
@@ -7,17 +7,22 @@
 # 🎓 Source: Coursera - R Programming - 第1單元 - Textual Data Formats
 ##################################################
 
-# ✏️ 說明區：
-# 這一區你可以簡單寫下這次練習的背景或想練習的語法重點。
-# 例如：使用 list 儲存一筆傷害紀錄，並練習 list 的命名與取值方式。
+# 使用情境
+  # 我是一間運動防護是的head AT，我要將暫時代理的運動傷害紀錄資料移轉給新的防護員，
+  # 這筆資料的欄位包含各種不同型別的數值，為了確保資料移轉的過程中不會格式轉換錯誤
+  # 練習用dput來寫入資料，並用dget讀取，確認型別是否正確
+
+
 
 # 設定工作目錄
 setwd("~/Documents/R practice")
-
 # 執行整個檔案
 source("injury_log_dput_practice.R")  
 
+# 寫入資料
 dput(injury_log, file = "injury_log.R")
+
+# 新的防護員讀取資料的方式
 injury_new <- dget("injury_log.R")
 
 # 測試
